f9141948241ec5182b5a68f60973e3fdf7c5df0c
KeVelo <kevkevinsango@gmail.com>
2024-03-17 20:59:20 -0600

    Guia 1

---
+++ app/src/main/java/sv/edu/ufg/fis/amb/practica/ActivitySecundaria.kt
@@ -0,0 +1,23 @@
+package sv.edu.ufg.fis.amb.practica
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+
+class ActivitySecundaria : AppCompatActivity() {
+    private lateinit var botonAtras: Button
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_secundaria)
+
+        botonAtras= findViewById(R.id.btn_volver)
+
+        botonAtras.setOnClickListener {
+            finish()
+        }
+
+    }
+}
